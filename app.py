60190247bc023adcab9befceca38132c933e415c
er1ck1h0r0 <162365651+er1ck1h0r0@users.noreply.github.com>
2024-08-06 08:08:03 +0300

    Update app.py

--- app.py
+++ app.py
@@ -857,4 +857,5 @@ swaggerui_blueprint = get_swaggerui_blueprint(SWAGGER_URL, API_URL, config={'app
 app.register_blueprint(swaggerui_blueprint, url_prefix=SWAGGER_URL)
 
 if __name__ == "__main__":
-    app.run(port=5555, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
